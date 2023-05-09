f6d52e9c6d022c2d91eb6efa8c0892aeee7a6877
Amber Moody <aperkins0922@gmail.com>
2023-05-09 11:58:59 -0400

    updated

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  
+add(item) {
+  this.items.push(item);
+  this.length++;
 
-  get(pos) {}
+  // Sort the items array in ascending order
+  this.items.sort((a, b) => a - b);
+}
 
-  max() {}
 
-  min() {}
+get(pos) {
+  if (pos >= 0 && pos < this.length) {
+    return this.items[pos];
+  } else {
+    throw new Error('OutOfBounds');
+  }
+}
+
+
+max() {
+  if (this.length === 0) {
+    throw new Error('EmptySortedList');
+  }
+  return this.items[this.length - 1];
+}
+
+
+min() {
+  if (this.length === 0) {
+    throw new Error('EmptySortedList');
+  }
+  return this.items[0];
+}
 
   sum() {}
 
